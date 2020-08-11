362476790ee6b2d8c9570b44ad9e01be97e71e7a
Lucas Johnson <woodsman.lucas@gmail.com>
2020-08-11 09:27:55 -0700

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Lucas Johnson on 2020-08-11.
+//  Copyright © 2020 Lucas Johnson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
